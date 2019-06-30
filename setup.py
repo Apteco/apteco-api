91312867463120115390ba0f1d28802d6f7f4dc5
Tim Morris <tim.morris@apteco.com>
2019-07-01 00:11:33 +0100

    Change long description to README; add author; add .api and .models as sub-packages (fix import issue); add .gitignore & setup.py to .openapi-generator-ignore to preserve edits

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "apteco-api"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 # To install the library, run the following
 #
 # python setup.py install
@@ -28,14 +28,14 @@ setup(
     name=NAME,
     version=VERSION,
     description="Apteco API",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://www.apteco.com/",
+	author="Apteco Ltd",
     author_email="support@apteco.com",
-    url="",
+	license="Apache 2.0",
     keywords=["OpenAPI", "OpenAPI-Generator", "Apteco API"],
     install_requires=REQUIRES,
-    packages=["apteco_api"],
+    packages=["apteco_api", "apteco_api.api", "apteco_api.models"],
     include_package_data=True,
-    long_description="""\
-    An API to allow access to Apteco Marketing Suite resources  # noqa: E501
-    """,
-	license="Apache 2.0",
 )
