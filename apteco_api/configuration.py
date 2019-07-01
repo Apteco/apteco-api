78ddb7b592c420a83d52a2e931bd8824710b1988
Tim Morris <tim.morris@apteco.com>
2019-07-01 15:07:01 +0100

    Revert "Fix tabs instead of spaces"
    This reverts commit 3241fe9d88457a47ea0ba841439f5463a30a54cc.

--- apteco_api/configuration.py
+++ apteco_api/configuration.py
@@ -62,13 +62,13 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
         """Temp file folder for downloading files
         """
         # Authentication Settings
-        if api_key is None:
-            api_key = {}
+		if api_key is None:
+		    api_key = {}
         self.api_key = api_key
         """dict to store API key(s)
         """
-        if api_key_prefix is None:
-            api_key_prefix = {}
+		if api_key_prefix is None:
+		    api_key_prefix = {}
         self.api_key_prefix = api_key_prefix
         """dict to store API prefix (e.g. Bearer)
         """
