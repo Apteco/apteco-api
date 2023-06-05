35cce9525f4b3faa4902c33f396a00acc5d935f2
Tim Morris <tim.morris@apteco.com>
2023-06-05 15:46:27 +0100

    Bump package patch number. [AB#14990]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "apteco-api"
-VERSION = "0.2.1"
+VERSION = "0.2.2"
 README = "introduction.md"
 
 # read the contents of your README file
