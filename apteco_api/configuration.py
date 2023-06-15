a092ce736bda9c1d873b5ad40eb36c6ea9505f23
Tim Morris <tim.morris@apteco.com>
2023-06-15 17:22:11 +0100

    Bump package number. [AB#15188]

--- apteco_api/configuration.py
+++ apteco_api/configuration.py
@@ -353,7 +353,7 @@ conf = apteco_api.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2\n"\
-               "SDK Package Version: 0.2.5".\
+               "SDK Package Version: 0.2.6".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
